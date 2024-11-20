7e1e1b582e9151d2d52c997b7487cc87bec84d5c
Froesv85 <vinicius.froes@catolicasc.edu.br>
2024-11-20 20:27:24 -0300

    V.1.0
    Correção na autenticação

--- android/app/src/main/kotlin/com/example/app_auth_firebase/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/app_auth_firebase/MainActivity.kt
@@ -1,5 +1,84 @@
 package com.example.app_auth_firebase
 
+import android.content.Intent
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.app_auth_firebase/auth"
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+
+        flutterEngine?.let {
+            MethodChannel(it.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+                if (call.method == "signOut") {
+                    signOut()
+                    result.success(null)
+                } else {
+                    result.notImplemented()
+                }
+            }
+        }
+    }
+
+    private fun signOut() {
+        auth.signOut()
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = auth.currentUser
+                    // Update UI
+                } else {
+                    // If sign in fails, display a message to the user.
+                    // Update UI
+                }
+            }
+    }
+
+    private fun signIn() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                // Update UI
+            }
+        }
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
+    }
+}
--- lib/viewmodels/auth_viewmodel.dart
+++ lib/viewmodels/auth_viewmodel.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import '../services/auth_service.dart';
 
 class AuthViewModel extends ChangeNotifier {
@@ -8,8 +10,24 @@ class AuthViewModel extends ChangeNotifier {
   User? get currentUser => _authService.getCurrentUser();
 
   Future<User?> signInWithGoogle(BuildContext context) async {
-    User? user = await _authService.signInWithGoogle();
-    return user;
+    try {
+      final User? user = await _authService.signInWithGoogle();
+      if (user == null) {
+        return null; // O usuário cancelou o login ou houve um erro
+      }
+
+      await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
+        'uid': user.uid,
+        'displayName': user.displayName,
+        'email': user.email,
+        'photoURL': user.photoURL,
+      });
+
+      return user;
+    } catch (e) {
+      print('Erro ao fazer login com Google: $e');
+      return null;
+    }
   }
 
   Future<void> signOut(BuildContext context) async {
